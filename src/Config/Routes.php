d76091580b2c2f111d6e0e2cee163053b0f3dd61
marshmallowfox <shudaof@gmail.com>
2024-09-30 11:35:16 +0600

    test

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -18,38 +18,38 @@ $routes->group('api', static function (RouteCollection $routes) {
     $routes->group('admin', static function (RouteCollection $routes) {
         $routes->group('blog', static function (RouteCollection $routes) {
             $routes->group('category', static function (RouteCollection $routes) {
-                $routes->get('/', [Category::class, 'index'], ['priority' => 10003]);
-                $routes->post('/', [Category::class, 'create'], ['priority' => 10003]);
-                $routes->put('(:num)', [[Category::class, 'update'], '$1'], ['priority' => 10003]);
-                $routes->delete('(:num)', [[Category::class, 'delete'], '$1'], ['priority' => 10003]);
+                $routes->get('/', [Category::class, 'index']);
+                $routes->post('/', [Category::class, 'create']);
+                $routes->put('(:num)', [[Category::class, 'update'], '$1']);
+                $routes->delete('(:num)', [[Category::class, 'delete'], '$1']);
             });
 
             $routes->group('tags', static function (RouteCollection $routes) {
-                $routes->get('/', [Tags::class, 'index'], ['priority' => 10003]);
-                $routes->put('(:num)', [[Tags::class, 'update'], '$1'], ['priority' => 10003]);
-                $routes->post('/', [Tags::class, 'create'], ['priority' => 10003]);
-                $routes->delete('(:num)', [[Tags::class, 'delete'], '$1'], ['priority' => 10003]);
+                $routes->get('/', [Tags::class, 'index']);
+                $routes->put('(:num)', [[Tags::class, 'update'], '$1']);
+                $routes->post('/', [Tags::class, 'create']);
+                $routes->delete('(:num)', [[Tags::class, 'delete'], '$1']);
             });
 
             $routes->group('post', static function (RouteCollection $routes) {
-                $routes->get('/', [Posts::class, 'index'], ['priority' => 10003]);
-                $routes->get('(:num)', [[Posts::class, 'edit'], '$1'], ['priority' => 10003]);
-                $routes->put('(:num)', [[Posts::class, 'update'], '$1'], ['priority' => 10003]);
-                $routes->put('upload/(:num)', [[Posts::class, 'upload'], '$1'], ['priority' => 10003]);
-                $routes->post('/', [Posts::class, 'create'], ['priority' => 10003]);
-                $routes->delete('(:num)', [[Posts::class, 'delete'], '$1'], ['priority' => 10003]);
+                $routes->get('/', [Posts::class, 'index']);
+                $routes->get('(:num)', [[Posts::class, 'edit'], '$1']);
+                $routes->put('(:num)', [[Posts::class, 'update'], '$1']);
+                $routes->put('upload/(:num)', [[Posts::class, 'upload'], '$1']);
+                $routes->post('/', [Posts::class, 'create']);
+                $routes->delete('(:num)', [[Posts::class, 'delete'], '$1']);
             });
 
             $routes->group('settings', static function (RouteCollection $routes) {
-                $routes->get('/', [Settings::class, 'index'], ['priority' => 10003]);
-                $routes->put('/', [Settings::class, 'update'], ['priority' => 10003]);
+                $routes->get('/', [Settings::class, 'index']);
+                $routes->put('/', [Settings::class, 'update']);
             });
         });
     });
 });
 
 $routes->group('blog', static function (RouteCollection $routes) {
-    $routes->get('/', [Blog::class, 'index'], ['priority' => 10003]);
-    $routes->get('post/(:segment)', [[Post::class, 'index'], '$1'], ['priority' => 10003]);
-    $routes->get('(:segment)', [[FrontCategory::class, 'index'], '$1'], ['priority' => 10003]);
+    $routes->get('/', [Blog::class, 'index']);
+    $routes->get('post/(:segment)', [[Post::class, 'index'], '$1']);
+    $routes->get('(:segment)', [[FrontCategory::class, 'index'], '$1']);
 });
